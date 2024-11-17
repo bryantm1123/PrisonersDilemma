98e91a423d6eec726c4e8e14c2b6caf24f7f6f8c
Matt <bryantm1122@gmail.com>
2024-11-17 12:31:37 +0100

    Initial Commit

---
+++ PrisonersDilemma/PrisonersDilemmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrisonersDilemmaApp.swift
+//  PrisonersDilemma
+//
+//  Created by Matt Bryant on 11/17/24.
+//
+
+import SwiftUI
+
+@main
+struct PrisonersDilemmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PrisonersDilemmaTests/PrisonersDilemmaTests.swift
@@ -0,0 +1,17 @@
+//
+//  PrisonersDilemmaTests.swift
+//  PrisonersDilemmaTests
+//
+//  Created by Matt Bryant on 11/17/24.
+//
+
+import Testing
+@testable import PrisonersDilemma
+
+struct PrisonersDilemmaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
